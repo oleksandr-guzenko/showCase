801857d45859b6ed1848a18b62ef20006c7166e1
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2021-03-03 08:39:38 +0700

    Delete server/config directory

--- server/config/firebaseAdminConfig.js
+++
@@ -1,8 +0,0 @@
-const serviceAccount = require("./serviceAccountKey.json");
-const admin = require("firebase-admin");
-
-const firebaseAdminConfig = {
-  credential: admin.credential.cert(serviceAccount),
-};
-
-module.exports = firebaseAdminConfig;
